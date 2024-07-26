1d09e52e3abbcba43442b8c55f0ef7ddd55e6513
darksidePardo <jjpardo2002@gmail.com>
2024-07-25 21:20:21 -0500

    feat: :sparkles: New features
    New features classes

--- src/main/java/com/asociacionesapp/app_relationship/entities/Client.java
+++ src/main/java/com/asociacionesapp/app_relationship/entities/Client.java
@@ -1,5 +1,63 @@
 package com.asociacionesapp.app_relationship.entities;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.Address;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "client_id")
+    private List<Address> addresses;
+
+    public Client() {
+    }
+    public Client(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "{id=" + id +
+                ", name=" + name +
+                ", lastname=" + lastname +
+                ", addresses=" + addresses + "}";
+    }
 
 }
--- src/main/java/com/asociacionesapp/app_relationship/entities/Invoice.java
+++ src/main/java/com/asociacionesapp/app_relationship/entities/Invoice.java
@@ -1,5 +1,54 @@
 package com.asociacionesapp.app_relationship.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="invoices")
 public class Invoice {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Long total;
+
+     // @JoinColumn(name = "client_id")
+    @ManyToOne
+    private Client client;
+
+    public Invoice() {
+    }
+    public Invoice(String description, Long total) {
+        this.description = description;
+        this.total = total;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Long getTotal() {
+        return total;
+    }
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
+    }
 
 }
